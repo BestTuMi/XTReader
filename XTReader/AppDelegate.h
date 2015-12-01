fc2790ae076c2674aa454960b1cfdf0c3aba2b48
gao7 <gao7@gao7deimac.local>
2015-12-01 15:10:56 +0800

    Initial Commit

---
+++ XTReader/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  XTReader
+//
+//  Created by gao7 on 15/12/1.
+//  Copyright © 2015年 gao7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ XTReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XTReader
+//
+//  Created by gao7 on 15/12/1.
+//  Copyright © 2015年 gao7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
